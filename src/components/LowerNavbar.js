30b43498cd099f138cf03b191607bbe50bd45ce2
Draken-Dragon <bravotspiderman@gmail.com>
2024-05-07 13:24:57 +0530

    Add Carousel.

--- src/components/LowerNavbar.js
+++ src/components/LowerNavbar.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { useSwipeable } from "react-swipeable";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 import rooms from "../images/rooms.jpg";
 import beach from "../images/beach.jpg";
 import lake from "../images/lake.jpg";
@@ -23,317 +25,140 @@ import Mountains from "../images/Mountains.png";
 import "../styles/lowerNavbar.css";
 
 const LowerNavbar = ({ setSelectedCategory }) => {
-  const btnpressprev = () => {
-    // const box = document.getElementById('box'); // Get the element by its id
-    let box = document.querySelector(".product-container");
-
-    if (box) {
-      const width = box.clientWidth;
-      box.scrollLeft -= width; // Use -= to scroll to the left
-      console.log(width);
-    } else {
-      console.error('Element with id "box" not found.');
-    }
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 10,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
 
-  const btnpressnext = () => {
-    // const box = document.getElementById('box'); // Get the element by its id
-    let box = document.querySelector(".product-container");
-
-    if (box) {
-      const width = box.clientWidth;
-      box.scrollLeft += width; // Use += to scroll to the right
-      console.log(width);
-    } else {
-      console.error('Element with id "box" not found.');
-    }
-  };
-
-  const handleSwipe = (direction) => {
-    console.log(`Swiped ${direction}`);
-    if (direction === "left") {
-      btnpressnext();
-    } else if (direction === "right") {
-      btnpressprev();
-    }
-  };
-
-  // Use the useSwipeable hook to add swipe functionality
-  const handlers = useSwipeable({
-    onSwipedLeft: () => handleSwipe("left"),
-    onSwipedRight: () => handleSwipe("right"),
-  });
   return (
-    <div className="product-carousel-1">
+    <Carousel responsive={responsive} containerClass="carousel">
+      <div className="carousel-item" onClick={() => setSelectedCategory("All")}>
+        <img src={world} alt="First slide" />
+        <p>All</p>
+      </div>
       <div
-        className="pre-btn-1"
-        onClick={(e) => {
-          btnpressprev();
-          e.stopPropagation();
-        }}
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Beachfront")}
       >
-        <p>
-          <img
-            src={less}
-            style={{ width: "22px", transform: "rotate(0deg)" }}
-          />
-        </p>
+        <img src={beach} alt="First slide" />
+        <p>Beachfront</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Desert")}
+      >
+        <img src={desert} alt="First slide" />
+        <p>Desert</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("lake")}
+      >
+        <img src={lake} alt="First slide" />
+
+        <p>Farms</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Ski-in/out")}
+      >
+        <img src={trending} alt="First slide" />
+
+        <p>Ski-in/out</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Camping")}
+      >
+        <img src={camping} alt="First slide" />
+
+        <p>Camping</p>
+      </div>
+      <div className="carousel-item">
+        <img src={mansion} alt="First slide" />
+
+        <p>Castles</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Countryside")}
+      >
+        <img src={Mountains} alt="First slide" />
+
+        <p> Countryside</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Arctic")}
+      >
+        <img src={rooms} alt="First slide" />
+
+        <p>Arctic</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Lakefront")}
+      >
+        <img src={Beach} alt="First slide" />
+
+        <p>Lakefront</p>
+      </div>
+      <div className="carousel-item">
+        <img src={CityView} alt="First slide" />
+
+        <p>Iconic Cities</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Caves")}
+      >
+        <img src={Heritage} alt="First slide" />
+
+        <p>Caves</p>
+      </div>
+      <div className="carousel-item">
+        <img src={TreeHouse} alt="First slide" />
+
+        <p>Barns</p>
+      </div>
+      <div
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Barns")}
+      >
+        <img src={SunView} alt="First slide" />
+
+        <p>Amazing Pool</p>
       </div>
 
       <div
-        className="product-container"
-        {...handlers}
-        id="carousel-prod-container"
+        className="carousel-item"
+        onClick={() => setSelectedCategory("Luxury")}
       >
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={world}
-              alt="First slide"
-              onClick={() => setSelectedCategory("All")}
-            />
-          </div>
-          <div className="text-container">
-            <p>All</p>
-          </div>
-        </div>
+        <img src={Luxe} alt="First slide" />
 
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={beach}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Beachfront")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Beachfront</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={desert}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Desert")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Desert</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={lake}
-              alt="First slide"
-              // onClick={() => handleProductClick2(3)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Farms</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={trending}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Ski-in/out")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Ski-in/out</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={camping}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Camping")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Camping</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={mansion}
-              alt="First slide"
-              // onClick={() => handleProductClick5(6)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Castles</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={Mountains}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Countryside")}
-            />
-          </div>
-          <div className="text-container">
-            <p> Countryside</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={rooms}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Arctic")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Arctic</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={Beach}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Lakefront")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Lakefront</p>
-          </div>
-        </div>
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={CityView}
-              alt="First slide"
-              // onClick={() => handleProductClick7(8)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Iconic Cities</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={Heritage}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Caves")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Caves</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={TreeHouse}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Barns")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Barns</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={SunView}
-              alt="First slide"
-              // onClick={() => handleProductClick7(8)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Amazing Pool</p>
-          </div>
-        </div>
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={Luxe}
-              alt="First slide"
-              onClick={() => setSelectedCategory("Luxury")}
-            />
-          </div>
-          <div className="text-container">
-            <p>Luxury</p>
-          </div>
-        </div>
-
-        {/* <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={BedBreakFast}
-              alt="First slide"
-              // onClick={() => handleProductClick7(8)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Bed & Breakfast</p>
-          </div>
-        </div> */}
-
-        <div className="carousel-1">
-          <div className="img-container">
-            <img
-              className="d-block-1 mycard-1"
-              src={Tropical}
-              alt="First slide"
-              // onClick={() => handleProductClick7(8)}
-            />
-          </div>
-          <div className="text-container">
-            <p>Islands</p>
-          </div>
-        </div>
+        <p>Luxury</p>
       </div>
-      <div
-        className="next-btn-1"
-        onClick={(e) => {
-          btnpressnext();
-          e.stopPropagation();
-        }}
-      >
-        <p>
-          <img src={less} style={{ width: "22px", transform: "scaleX(-1)" }} />
-        </p>
+      <div className="carousel-item">
+        <img src={BedBreakFast} alt="First slide" />
+
+        <p>Bed & Breakfast</p>
       </div>
-    </div>
+      <div className="carousel-item">
+        <img src={Tropical} alt="First slide" />
+
+        <p>Islands</p>
+      </div>
+    </Carousel>
   );
 };
 
